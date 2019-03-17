93a04b2cdfa155ddee8200ffd8171c37c2ba8f47
hisasann <hisasann25@gmail.com>
2019-03-17 17:25:21 +0900

    feat: add cli.js

--- cli.js
+++ cli.js
@@ -1,3 +1,17 @@
-'use strict';
+#!/usr/bin/env node
+const path = require('path')
+const sao = require('sao')
 
-console.log('create-ts-nuxt-page');
+const generator = path.resolve(__dirname, './')
+// In a custom directory or current directory
+const outDir = path.resolve(process.argv[2] || '.')
+
+console.log(`> Generating Nuxt.js page in ${outDir}`)
+
+// See https://sao.js.org/#/advanced/standalone-cli
+sao({ generator, outDir, logLevel: 2 })
+  .run()
+  .catch((err) => {
+    console.trace(err)
+    process.exit(1)
+  })
